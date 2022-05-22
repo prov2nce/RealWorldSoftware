d7d7ce55fa23dd758278d15d1ce76fc473988d06
prov2nce <zzir8534@naver.com>
2022-05-23 02:07:29 +0900

    chapter_04

---
+++ src/main/java/com/chapter_4/ImageImporter.java
@@ -0,0 +1,28 @@
+package com.chapter_4;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import javax.imageio.ImageIO;
+import com.chapter_4.interf.Importer;
+
+import static com.chapter_4.Attributes.*;
+
+public class ImageImporter implements Importer {
+	
+	@Override
+	public Document importFile(File file) throws IOException {
+		final Map<String, String> attributes = new HashMap<>();
+		attributes.put(PATH,  file.getPath());
+		
+		final BufferedImage image = ImageIO.read(file);
+		attributes.put(WIDTH, String.valueOf(image.getWidth()));
+		attributes.put(HEIGHT, String.valueOf(image.getHeight()));
+		attributes.put(TYPE, "IMAGE");
+		
+		return new Document(attributes);
+	}
+
+}
